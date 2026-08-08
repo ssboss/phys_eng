87995648d56a5c33f1ac1c43baaf3104003fc971
ssboss <12957633+ssboss@users.noreply.github.com>
2026-08-07 21:08:34 -0400

    Implemented Adapter member functions in World and added object slots

--- src/world/world.h
+++ src/world/world.h
@@ -1,30 +1,32 @@
-#include "vector3D.h"
-#include "particle.h"
+#include "core.h"
 #include "dynamics.h"
 #include "kinematics.h"
-#include <vector>
 #include <algorithm>
 
-class World {
+struct ObjHandle {
+    Physics::Particle* obj;
+    u_int32_t generation;
+};
+
+class World : public RenderInfo {
     public:
         World(Physics::Vector3D upperBound, Physics::Vector3D lowerBound, float dt);
         ~World();
-        void update();
+        void update() override;
         void modUpBounds(Physics::Vector3D low);
         void modLowBounds(Physics::Vector3D high);
-        Physics::Vector3D getUpperBounds() const;
-        Physics::Vector3D getLowerBounds() const;
+        const Physics::Vector3D getUpperBounds() const override;
+        const Physics::Vector3D getLowerBounds() const override;
         void addForce(Physics::Vector3D force);
         void addObj(Physics::Particle* obj);
-        void remObj(Physics::Particle* obj);
-        const std::vector<Physics::Particle*>* getObjs() const;
-
+        void remObj(int id);
+        const Physics::Vector3D getPos(int id) const override;
+        const float getRadius(int id) const override;
+        const unsigned int getCount() const override;
     private:
         std::vector<Physics::Vector3D> forces; // global forces that affect the world
-        std::vector<Physics::Particle*> objs;
-        Physics::Vector3D worldMin;
-        Physics::Vector3D worldMax;
+        std::vector<ObjHandle> objs;
+        Physics::Vector3D worldMin, worldMax;
         float dt;
-
-        
+        int width, height;
 };
